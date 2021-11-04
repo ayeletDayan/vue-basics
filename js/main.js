47c9397e6d334dd456d8eaa3bfd5a337605e90a8
Ayelet Dayan <ayeletgeri@yahoo.com>
2021-11-04 13:26:48 +0200

    1st commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+// import './cmps/simple-counter.cmp.js'
+
+
+const options = {
+    el: '#app',
+    data() {
+        return {
+            msg: 'Hola!',
+        }
+    },
+    methods: {
+    },
+    computed: {
+    }
+}
+
+new Vue(options)
