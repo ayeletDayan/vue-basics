9b16aa106d68ede146b1b054bc1cd180b705bad9
Ayelet Dayan <ayeletgeri@yahoo.com>
2021-11-05 10:37:45 +0200

    final commit

--- js/cmps/user-preview.cmp.js
+++ js/cmps/user-preview.cmp.js
@@ -1,28 +1,37 @@
 Vue.component('user-preview', {
     props: ['user'],
     template: `
-    <section class="user-preview">
-    <img v-bind:src="{{user.img}}"/>
-    <h4>{{user.name}}</h4>
-    </section>
+    <div class="user-preview">
+    <img v-on:click="setTvShows" v-bind:src="user.img">    
+    <h4 v-on:click="setNickname">{{user.name}}</h4>
+    <div v-bind:class="getMode">{{showUserTv}}</div>
+    </div>    
     `,
+
     data() {
         return {
-            // img: getImg()
+            isShown: false,            
         }
     },
-    // Life cycle hooks
-    created() {
-        console.log('User Preview Created!');
-    },    
-    destroyed() {
-        console.log('User Preview Destroyed!');
-    },
-      
+    
     methods: {  
-        // getImg(){
-        //     return 'img/1.jpg';
-        // },   
+        setNickname(){
+            console.log('nickname');
+            this.user.name = prompt('Set Nickname')
+        },
 
+        setTvShows(){
+            console.log('TV');
+            this.isShown = !this.isShown;            
+        }
+
+    },
+    computed: {
+        getMode() {
+            return this.isShown ? 'show-tv' : 'hide-tv';
+        },
+        showUserTv(){
+            return this.user.tvShows.join()
+        }
     }
 })
--- js/cmps/whowatch.cmp.js
+++ js/cmps/whowatch.cmp.js
@@ -2,15 +2,17 @@ import './user-preview.cmp.js'
 
 Vue.component('who-watch', {
     template: `<section class="user-list">                
-                    <div v-for="(currUser, idx) in users">
-                        <user-preview v-bind:user="currUser"></user-preview>
-                        <button v-on:click="removeUser(currUser.id)">x</button>
+                    <div class="user" v-for="(currUser, idx) in users">
+                    <user-preview v-bind:user="currUser"></user-preview>
+                        <button class="remove" v-on:click="removeUser(currUser.id)">x</button>
                     </div>                
-                    <button v-on:click="addUser">Add User</button>
-                </section>        `,
+                    <button class="add" v-on:click="addUser">Add User</button>
+                </section>
+                <div class="tv-shows"
+                `,
     data() {
         return {
-            users: [{ id: 'p101', name: 'Shuki' , img: 'img/1.jpg'}, { id: 'p102', name: 'Puki' , img: 'img/2.jpg'}, { id: 'p103', name: 'Muki' , img: 'img/3.jpg'}],
+            users: [{ id: 'p101', name: 'Shuki' , img: 'img/1.jpg', tvShows: ['tv1', 'tv2']}, { id: 'p102', name: 'Puki' , img: 'img/2.jpg', tvShows: ['tv5', 'tv7'] }, { id: 'p103', name: 'Muki' , img: 'img/3.jpg', tvShows: ['tv1', 'tv5']}],
         }
     },
     methods: {
@@ -20,7 +22,7 @@ Vue.component('who-watch', {
         },
         addUser() {
             const num = Date.now() % 1000
-            this.users.push({ id: 'p' + num, name: `User ${num}` })
+            this.users.push({ id: 'p' + num, name: `User ${num}`})
         }
 
     }
